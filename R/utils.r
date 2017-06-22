ed61799026f2f22cfa78f80050275d294ee5c24f
lefft <tjleffel@gmail.com>
2017-06-21 19:56:27 -0500

    add a few more ones i use a lot and a readme

--- R/utils.r
+++ R/utils.r
@@ -1,4 +1,140 @@
-### commonly used utility functions
+### functions for data analysis (updated jun21/2017)
+
+## wishlist: 
+# 
+#   - center a variable around zero
+#   - bootstrap regression coefficients
+#   - simple permutation test func
+#   - more general perm func
+#   - ggplot2 theme(s?)
+#   - want to represent `colz()` as data not fonc
+#   - ...
+
+
+# calculate 95% confidence intervals via non-parametric bootstrap 
+#' bootstrap mean and other summary stats
+#'
+#' @param vec 
+#' @param b 
+#' @param n 
+#' @param dig 
+#' @param narm 
+#'
+#' @return val 
+#' @export
+#'
+#' @examples boosh 
+bootCI <- function(vec, b=1000, n=length(vec[!is.na(vec)]), dig=6, narm=TRUE){
+  # can remove missings (build in logic for missings later)
+  vec <- vec[!is.na(vec)]
+  # make a container for results
+  boots <- rep(NA, times=b) 
+  # conduct bootstrap resampling
+  for (x in 1:b){
+    boots[x] <- mean(sample(vec, size=n, replace=TRUE), na.rm=narm)
+  }
+  # boot mean is mean of the boots
+  bootMean <- mean(boots, na.rm=narm)
+  # boot std error is sd of the boot means
+  bootSE <- sd(boots, na.rm=narm)
+  # boot ci's are +/-2 boot se's
+  bootLo <- mean(vec, na.rm=narm) - 1.96*bootSE
+  bootHi <- mean(vec, na.rm=narm) + 1.96*bootSE
+  # collect results + return them
+  return(round(c(
+    "bootMean"=bootMean,
+    "sampMean"=mean(vec),
+    "bootSE"=bootSE,
+    "sampSD"=sd(vec),
+    "bootLo"=bootLo,
+    "bootHi"=bootHi
+  ), digits=dig))
+}
+
+# standard error of a sample proportion
+#' se of a proportion
+#'
+#' @param p 
+#' @param n 
+#'
+#' @return val 
+#' @export
+#'
+#' @examples boosh 
+se_prop <- function(p, n){
+  round(sqrt((p*(1-p))/n), digits=2)
+}
+
+# reasonable color palette for plotting
+#' colorze
+#'
+#' @return val 
+#' @export
+#'
+#' @examples boosh 
+colz <- function(){
+  c(lite_tan="#dbd4d0", claudy_braun="#afa69e", afghan_tan="#c8ab94",
+    baygish="#68442a", bilbao="#248100", japlore="#2F742C")
+}
+
+
+# (equiv to a wrapper around `scale()` w defaults, but returns atomic vec)
+#' quick zscore func 
+#'
+#' @param x 
+#'
+#' @return val 
+#' @export
+#'
+#' @examples boosh 
+zscore <- function(x){
+  (x - mean(x, na.rm=TRUE)) / sd(x, na.rm=TRUE)
+}
+
+
+
+### simple + commonly used utility functions
+
+# attach packages w/o annoying messages
+#' quiet_attach
+#'
+#' @param packages 
+#' @param shhh 
+#'
+#' @return val 
+#' @export
+#'
+#' @examples boosh 
+quiet_attach <- function(packages, shhh=TRUE){
+  for (x in seq_along(packages)){
+    if (shhh==TRUE){
+      suppressPackageStartupMessages(
+        do.call(library, list(packages[x]))
+      )
+    } else {
+      do.call(library, list(packages[x]))
+    }
+  }
+}
+
+
+# annotate pvals w stars
+#' pvalClassify
+#'
+#' @param x 
+#' @param alpha 
+#'
+#' @return val 
+#' @export
+#'
+#' @examples boosh 
+pvalClassify <- function(x, alpha=.05){
+  ifelse(x < (alpha/50), " (***)", 
+         ifelse(x < (alpha/5), " (**)", 
+                ifelse(x < alpha, " (*)", 
+                       ifelse(x < (alpha*2), " +", " n.s."))))
+}
+
 
 #' string position getter
 #'
---
+++ README.md
@@ -0,0 +1,16 @@
+package that will contain some useful stuff i use regularly. data-loading functionality doesn't work yet if this sentence is still here. 
+
+-tim 
+
+<hr><br>
+
+usage: 
+
+```r
+# install 
+devtools::install_github("lefft/lefftpack")
+
+# make functions available in session
+library("lefftpack")
+```
+
